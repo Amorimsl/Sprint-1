6683d06fa7b72bbffaab8ea148d688ec121a757b
lucas amorim <lucasamorim_10_@hotmail.com>
2024-05-28 20:29:56 -0300

    commit da page5 com html e css

--- main.js
+++ main.js
@@ -38,43 +38,16 @@ controls.forEach((control) => {
       items[startIndex].scrollIntoView({
         inline: 'start',
         behavior: 'smooth',
+        block: 'nearest',
       });
     } else {
       items[startIndex + itemsPerGroup - 1].scrollIntoView({
         inline: 'end',
         behavior: 'smooth',
+        block: 'nearest',
       });
     }
     // Marcar apenas o botão de rádio correspondente ao grupo atual
     radios[startIndex / itemsPerGroup].checked = true;
   });
 });
-
-//function nextOne() {
-//const slideDogOne = document.getElementById('slide_dog1');
-//const slideDogTwo = document.getElementById('slide-dog2');
-//const checkSlide = document.getElementById('slide1');
-
-//slideDogOne.classList.add('hidden');
-//checkSlide.checked = false;
-//slideDogTwo.classList.remove('hidden');
-//checkSlide.checked = true;
-//}
-
-//slides.addEventListener('click', nextOne);
-
-//function checkRadio() {
-//const radios = document.getElementsByName('radio-btn');
-//const slideDogTwo = document.getElementById('slide-dog2');
-//const slideDogOne = document.getElementById('slide_dog1');
-//radios.forEach((radio) => {
-//if (radio.checked) {
-//if (radio.id === 'slide2') {
-//slideDogOne.classList.add('hidden');
-//slideDogTwo.classList.remove('hidden');
-// }
-// console.log(`${radio.id} is checked`);
-//}
-//});
-//}
-//checkRadio();
