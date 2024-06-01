4983ba0ff8f53121ae8e12c930a1889f4d550c84
lucas amorim <lucasamorim_10_@hotmail.com>
2024-06-01 02:00:14 -0300

    commit da validacao de todos os inputs e checks

--- main.js
+++ main.js
@@ -4,6 +4,41 @@ const items = document.querySelectorAll('.item');
 const itemsPerGroup = 4; // Defina o número de itens por grupo
 let groupIndex = 0; // variavel que define
 
+const cancelApply = document.querySelector('#button-cancel-apply');
+const dialogApply = document.querySelector('.apply-dialog');
+const modal = document.querySelector('dialog');
+const buttonCancelDonate = document.getElementById('button-cancel-modal');
+console.log(cancelApply);
+
+const valueInputEmail = document.getElementById('email-input-donate');
+const msgErrorInput = document.getElementById('error-input-email');
+const valuePrice = document.getElementById('priceInput');
+const msgErrorPrice = document.getElementById('error-input-price');
+const buttonHelp = document.getElementById('button-confirm-help');
+const email = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+const radiosPayment = document.querySelectorAll(
+  '#radio-donate ,#radio-donate-credit ,#radio-donate-paypau'
+);
+const smallErrorPayment = document.getElementById('small-pix');
+
+const inputEmailAdopt = document.getElementById('email-adopt');
+const msgErrorAdopt = document.getElementById('error-input-email-adopt');
+const fullNameInput = document.getElementById('fullNameInput');
+const msgErrorName = document.getElementById('error-input-name');
+const buttonAdopt = document.getElementById('button-adopt');
+const fullName = /^[A-Za-zÀ-ÖØ-öø-ÿ]+([ \'-][A-Za-zÀ-ÖØ-öø-ÿ]+)*$/;
+const email2 = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+const RadioAgree = document.getElementById('radio-agree');
+const ErrorAgree = document.getElementById('small-agree');
+const dataDay = document.getElementById('day');
+const dataMonth = document.getElementById('month');
+const dataYear = document.getElementById('year');
+const smallErrorData = document.getElementById('error-data');
+
+const divs = document.querySelectorAll(
+  '#color-pix, #color-credit, #color-paypal'
+);
+
 const maxItems = items.length;
 
 controls.forEach((control) => {
@@ -52,12 +87,7 @@ const donate = document.querySelectorAll(
 const adopt = document.querySelectorAll(
   '#Apply-id  , #apply-page4 ,#button-last-page'
 );
-const cancelApply = document.getElementById('button-cancel-apply');
-const dialogApply = document.querySelector('.apply-dialog');
-//const donateHeader = document.getElementById('donate-id2');
-const modal = document.querySelector('dialog');
-const buttonCancelDonate = document.getElementById('button-cancel-modal');
-console.log(cancelApply);
+
 donate.forEach(function (el) {
   el.addEventListener('click', () => {
     modal.showModal();
@@ -71,6 +101,8 @@ buttonCancelDonate.onclick = function () {
   modal.close();
   modal.style.left = '2606px';
   document.body.classList.remove('modal-aberto');
+  //msgErrorInput.innerHTML = '';
+  //msgErrorPrice.innerHTML = '';
 };
 // show modal Apply to Adopt
 adopt.forEach(function (el) {
@@ -85,4 +117,137 @@ cancelApply.onclick = function () {
   dialogApply.close();
   dialogApply.style.left = '2606px';
   document.body.classList.remove('modal-aberto');
+  msgErrorAdopt.innerHTML = '';
+  msgErrorName.innerHTML = '';
 };
+
+/// validar
+
+radiosPayment.forEach((radio) => {
+  radio.addEventListener('change', function () {
+    divs.forEach((div, i) => {
+      if (radiosPayment[i].checked) {
+        div.classList.add('color-divs');
+      } else {
+        div.classList.remove('color-divs');
+      }
+    });
+  });
+});
+const iWannaHelp = () => {
+  buttonHelp.addEventListener('click', () => {
+    let isValidHelp = true;
+    if (valueInputEmail.value == 0) {
+      msgErrorInput.innerHTML = 'Adicione um email';
+      isValidHelp = false;
+      //msgErrorInput.style.color = 'red';
+    } else {
+      msgErrorInput.innerHTML = '';
+    }
+    if (valuePrice.value == 0) {
+      msgErrorPrice.innerHTML = 'Adicione um preço';
+      isValidHelp = false;
+    } else {
+      msgErrorPrice.innerHTML = '';
+    }
+    if (!email.test(valueInputEmail.value)) {
+      msgErrorInput.innerHTML = 'Adicione um email valido';
+      // msgErrorInput.style.color = 'red';
+      isValidHelp = false;
+    } else {
+      msgErrorInput.innerHTML = '';
+    }
+
+    let isChecked = false;
+    radiosPayment.forEach((el) => {
+      if (el.checked) {
+        isChecked = true;
+      }
+    });
+    if (!isChecked) {
+      smallErrorPayment.innerHTML = 'Selecione um método de pagamento!';
+      isValidHelp = false;
+    } else {
+      smallErrorPayment.innerHTML = '';
+    }
+
+    if (isValidHelp == true) {
+      location.href = 'success.html';
+    }
+  });
+};
+
+iWannaHelp();
+///
+
+const iWannaAdopt = () => {
+  let daySelect = dataDay.options[dataDay.selectedIndex].text;
+  let monthSelect = dataMonth.options[dataMonth.selectedIndex].text;
+  let yearSelect = dataYear.options[dataYear.selectedIndex].text;
+  buttonAdopt.addEventListener('click', () => {
+    let isValid = true;
+    if (inputEmailAdopt.value == 0) {
+      msgErrorAdopt.innerHTML = 'Digite um email';
+      inputEmailAdopt.focus();
+      isValid = false;
+    } else {
+      msgErrorAdopt.innerHTML = '';
+    }
+    if (email2.test(inputEmailAdopt.value) === false) {
+      console.log(email.test(inputEmailAdopt.value));
+      msgErrorAdopt.innerHTML = 'Digite um email valido';
+      inputEmailAdopt.focus();
+      isValid = false;
+    } else {
+      msgErrorAdopt.innerHTML = '';
+    }
+
+    if (!fullName.test(fullNameInput.value)) {
+      msgErrorName.innerHTML = 'Digite um nome valido';
+      fullNameInput.focus();
+      isValid = false;
+    } else {
+      msgErrorName.innerHTML = '';
+    }
+
+    if (!RadioAgree.checked) {
+      ErrorAgree.innerHTML = 'Voce precisa concordar com os termos';
+      RadioAgree.focus();
+      isValid = false;
+    } else {
+      ErrorAgree.innerHTML = '';
+    }
+
+    if (daySelect == 'Day' || monthSelect == 'Month' || yearSelect == 'Year') {
+      smallErrorData.innerHTML = 'Selecione uma Data';
+      isValid = false;
+    } else {
+      smallErrorData.innerHTML = '';
+    }
+
+    if (isValid == true) {
+      location.href = 'success.html';
+    }
+  });
+};
+
+iWannaAdopt();
+
+const buttonSubscribe = document.getElementById('button-Subscribe');
+const emailSubscribe = document.getElementById('email-Subscribe');
+const inputSmall = document.getElementById('small-subscribe');
+
+buttonSubscribe.addEventListener('click', () => {
+  let isValid2 = true;
+  if (email2.test(emailSubscribe.value) === false) {
+    inputSmall.innerHTML = 'Escreva um Email Valido';
+    inputSmall.focus();
+    isValid2 = false;
+  } else {
+    inputSmall.innerHTML = '';
+  }
+
+  if (isValid2 == true) {
+    location.href = 'success.html';
+  }
+});
---
+++ view.js
@@ -0,0 +1,87 @@
+const donate = document.querySelector('#button-donate-view');
+const modal = document.querySelector('dialog');
+const buttonCancelDonate = document.getElementById('button-cancel-modal');
+const valueInputEmail = document.getElementById('email-input-donate');
+const msgErrorInput = document.getElementById('error-input-email');
+const valuePrice = document.getElementById('priceInput');
+const msgErrorPrice = document.getElementById('error-input-price');
+const buttonHelp = document.getElementById('button-confirm-help');
+const email = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+const radiosPayment = document.querySelectorAll(
+  '#radio-donate ,#radio-donate-credit ,#radio-donate-paypau'
+);
+const smallErrorPayment = document.getElementById('small-pix');
+
+const divs = document.querySelectorAll(
+  '#color-pix, #color-credit, #color-paypal'
+);
+
+donate.onclick = function () {
+  modal.showModal();
+  document.body.classList.add('modal-aberto');
+  modal.style.left = '50%';
+};
+
+buttonCancelDonate.onclick = function () {
+  modal.close();
+  modal.style.left = '2606px';
+  document.body.classList.remove('modal-aberto');
+  //msgErrorInput.innerHTML = '';
+  //msgErrorPrice.innerHTML = '';
+};
+radiosPayment.forEach((radio) => {
+  radio.addEventListener('change', function () {
+    divs.forEach((div, i) => {
+      if (radiosPayment[i].checked) {
+        div.classList.add('color-divs');
+      } else {
+        div.classList.remove('color-divs');
+      }
+    });
+  });
+});
+const iWannaHelp = () => {
+  buttonHelp.addEventListener('click', () => {
+    let isValidHelp = true;
+    if (valueInputEmail.value == 0) {
+      msgErrorInput.innerHTML = 'Adicione um email';
+      isValidHelp = false;
+      //msgErrorInput.style.color = 'red';
+    } else {
+      msgErrorInput.innerHTML = '';
+    }
+    if (valuePrice.value == 0) {
+      msgErrorPrice.innerHTML = 'Adicione um preço';
+      isValidHelp = false;
+    } else {
+      msgErrorPrice.innerHTML = '';
+    }
+    if (!email.test(valueInputEmail.value)) {
+      msgErrorInput.innerHTML = 'Adicione um email valido';
+      // msgErrorInput.style.color = 'red';
+      isValidHelp = false;
+    } else {
+      msgErrorInput.innerHTML = '';
+    }
+
+    let isChecked = false;
+    radiosPayment.forEach((el) => {
+      if (el.checked) {
+        isChecked = true;
+      }
+    });
+
+    if (!isChecked) {
+      smallErrorPayment.innerHTML = 'Selecione um método de pagamento!';
+      isValidHelp = false;
+    } else {
+      smallErrorPayment.innerHTML = '';
+    }
+
+    if (isValidHelp == true) {
+      location.href = 'success.html';
+    }
+  });
+};
+
+iWannaHelp();
